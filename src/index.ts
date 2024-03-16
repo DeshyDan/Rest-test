0a25b403b7242769aa617945d2d15a6ec93f8818
Deshy Dan <deshy733@gmail.com>
2024-03-16 16:18:24 +0200

    Setup Mongo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello World")
+import express from 'express'
+import http from 'http'
+import bodyParser from "body-parser"
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import config from 'config'
+
+const app = express()
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+
+const server = http.createServer(app)
+
+server.listen(8080, () => {
+    console.log("Server is running on http://localhost:8080/")
+})
+
+const MONGO_URL: string = config.get("Mongo.URL")
+
+async function connectToMongo() {
+    try {
+
+        await mongoose.connect(MONGO_URL)
+        console.log("Successfully connected to Mongo")
+
+    } catch (err) {
+        console.log(`Could not connect to Mongo: ${err}`)
+    }
+}
+
+connectToMongo()
+
