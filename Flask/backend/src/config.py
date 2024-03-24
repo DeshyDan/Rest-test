4a6199156f52d63a26d691fb5b9ac04383f533bf
Deshy Dan <deshy733@gmail.com>
2024-03-24 03:08:52 +0200

    Add additional routes

--- Flask/backend/src/config.py
+++ Flask/backend/src/config.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://mydatabase.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
